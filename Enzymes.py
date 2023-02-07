451b7c5c4f986dc589576c32f31034749d02d1d7
IqbalNasar <120912796+IqbalNasar@users.noreply.github.com>
2023-02-07 21:47:40 +0500

    Update Enzymes.py

--- Enzymes.py
+++ Enzymes.py
@@ -95,6 +95,30 @@ print(y_orig)
 
 print(accuracy_score(y_orig, y_pred))
 
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+
+
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
 
 
 
